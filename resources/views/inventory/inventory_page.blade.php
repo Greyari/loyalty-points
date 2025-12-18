d0948d525377fd8864c8c8f71b10b31c47045f49
HaikalTaufiq <134383454+HaikalTaufiq@users.noreply.github.com>
2025-12-18 09:05:23 +0700

    Inventory -> product

--- resources/views/inventory/inventory_page.blade.php
+++ resources/views/inventory/inventory_page.blade.php
@@ -21,8 +21,8 @@
 <div class="space-y-6 mb-6">
     <div class="flex justify-between items-start">
         <div>
-            <h2 class="text-2xl sm:text-3xl lg:text-4xl font-semibold font-poppins mb-0">Inventory</h2>
-            <p class="text-sm sm:text-base lg:text-lg font-light text-gray-500 font-poppins">Manage your inventory.</p>
+            <h2 class="text-2xl sm:text-3xl lg:text-4xl font-semibold font-poppins mb-0">Product</h2>
+            <p class="text-sm sm:text-base lg:text-lg font-light text-gray-500 font-poppins">Manage your product.</p>
         </div>
         <button onclick="window.open_productImportModal()" class="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg transition-colors flex items-center gap-2 font-poppins">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
